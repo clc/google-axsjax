815faacf62b743ebe7c85dd482e6d66baf0f88ab
clchen@google.com <clchen@google.com@333d7aac-213b-0410-876c-dd2d68a01fb3>
2007-10-19 00:25:31 +0000

    Ads can now be read. Note that there will be alot of garbage being read because of a Fire Vox bug in how table row headers are guessed (ads use a layout table which is mistaken for a data table).

--- trunk/websearch/axsEnableWebSearch.js
+++ trunk/websearch/axsEnableWebSearch.js
@@ -14,15 +14,25 @@
 // create namespace
 var axsWebSearch = {};
 
+
+//These are strings to be spoken to the user
+axsWebSearch.NO_ONE_BOX_STRING = 'There is no one box on this page.';
+axsWebSearch.NO_ADS_STRING = 'There are no advertisments on this page.';
+
+
+
 /**
  * The AxsJAX object that will do the tickling and speaking.
  * @type AxsJAX
  */
 axsWebSearch.axsJAXObj = null;
 
+
+axsWebSearch.adAreaId = null;
+axsWebSearch.adIndex = 0;
 axsWebSearch.inputFocused = false;
 
-axsWebSearch.NO_ONE_BOX_STRING = 'There is no one box on this page.';
+
 
 
 
@@ -38,10 +48,14 @@ axsWebSearch.init = function(){
     window.content.document.location = locationString + '&esrch=BetaShortcuts';
   }
 
+  //Add event listeners
   window.addEventListener('DOMAttrModified', axsWebSearch.domAttrModifiedHandler, true);
   window.addEventListener('keypress', axsWebSearch.extraKeyboardNavHandler, true);
   window.addEventListener('focus', axsWebSearch.focusHandler, true);
   window.addEventListener('blur', axsWebSearch.blurHandler, true);
+
+  //Do any necessary preparations for browsing here
+  axsWebSearch.formatAdsArea();
 };
 
 /**
@@ -87,6 +101,9 @@ axsWebSearch.extraKeyboardNavHandler = function(evt){
   if (evt.charCode == 49){ // 1
     axsWebSearch.readOneBox();
   }
+  if (evt.charCode == 97){ // a
+    axsWebSearch.cycleThroughAds();
+  }
 };
 
 
@@ -98,6 +115,63 @@ axsWebSearch.readOneBox = function(){
   }
 };
 
+
+
+
+
+axsWebSearch.formatAdsArea = function(){
+  var adTable = window.content.document.getElementById('mbEnd');
+  var adArea = null; //This is the FONT tag that contains all the ads
+                     //However, it is non-trivial to locate as the TR
+                     //that it is under is not always the same TR.
+                     //There are also very few identifiers here to key off of.
+  //This is an ugly way to find the adArea, but it works reliably.
+  var spanArray = adTable.getElementsByTagName('SPAN');
+  for (var i=0; i<spanArray.length; i++){
+    if (spanArray[i].className == 'a'){
+      adArea = spanArray[i].parentNode;
+      break;
+    }
+  }
+  if (!adArea){
+    return;
+  }
+
+  var currentAdAnchor = adArea.firstChild;
+  var adSpan = window.content.document.createElement('span');
+
+  while (currentAdAnchor){
+    var nextAnchor = currentAdAnchor.nextSibling;
+    if (currentAdAnchor.tagName &&  currentAdAnchor.tagName == 'FONT'){
+      var newAdSpan = window.content.document.createElement('span');
+      newAdSpan.appendChild(currentAdAnchor.cloneNode(true));
+      adArea.replaceChild(adSpan,currentAdAnchor);
+      adSpan = newAdSpan;
+    } else {
+      adSpan.appendChild(currentAdAnchor.cloneNode(true));
+      adArea.removeChild(currentAdAnchor);
+    }
+    currentAdAnchor = nextAnchor;
+  }
+  adArea.appendChild(adSpan);
+  //Clean up by deleting the first child which is a blank node
+  adArea.removeChild(adArea.firstChild);
+  axsWebSearch.adAreaId = axsWebSearch.axsJAXObj.assignId(adArea);
+};
+
+
+axsWebSearch.cycleThroughAds = function(){
+  if (!axsWebSearch.adAreaId){
+    axsWebSearch.axsJAXObj.speakText(axsWebSearch.NO_ADS_STRING);
+    return;
+  }
+  var adArea = document.getElementById(axsWebSearch.adAreaId);
+  if (axsWebSearch.adIndex >= adArea.childNodes.length){
+    axsWebSearch.adIndex = 0;
+  }  
+  var currentAd = adArea.childNodes[axsWebSearch.adIndex++];
+  axsWebSearch.axsJAXObj.speakNode(currentAd);
+};
 
 
 
