214306a5f2faf33f3fad0f4d2661125dd2ed975c
tv.raman.tv <tv.raman.tv@333d7aac-213b-0410-876c-dd2d68a01fb3>
2007-10-31 16:35:59 +0000

    Fixed long lines. May need to refactor common base URL into a constant for hygene.

--- trunk/common/axsScriptChooser.js
+++ trunk/common/axsScriptChooser.js
@@ -13,7 +13,8 @@
 function pickScript(){
   var theLib = document.createElement('script');
   theLib.type = 'text/javascript';
-  theLib.src = 'http://google-axsjax.googlecode.com/svn/trunk/common/AxsJAX.js';
+  theLib.src =
+      'http://google-axsjax.googlecode.com/svn/trunk/common/AxsJAX.js';
   //Do not insert anything if the scripts are already inserted.
   var scriptArray = document.getElementsByTagName('script');
   for (var i=0; i<scriptArray.length; i++){
@@ -26,11 +27,15 @@ function pickScript(){
   theScript.type = 'text/javascript';
   var currentURL = document.baseURI;
   if (currentURL.indexOf('http://www.google.com/reader/') == 0){
-    theScript.src = 'http://google-axsjax.googlecode.com/svn/trunk/reader/axsEnableReader.js';
+    theScript.src =
+        'http://google-axsjax.googlecode.com/svn/trunk/reader/axsEnableReader.js';
     shouldInsertScripts = true;
   }
-  if ((currentURL == 'http://www.google.com/') || (currentURL.indexOf('http://www.google.com/search') == 0) || (currentURL.indexOf('http://www.google.com/custom') == 0)){
-    theScript.src = 'http://google-axsjax.googlecode.com/svn/trunk/websearch/axsEnableWebSearch.js';
+  if ((currentURL == 'http://www.google.com/')
+      || (currentURL.indexOf('http://www.google.com/search') == 0)
+      || (currentURL.indexOf('http://www.google.com/custom') == 0)){
+    theScript.src =
+        'http://google-axsjax.googlecode.com/svn/trunk/websearch/axsEnableWebSearch.js';
     shouldInsertScripts = true;    
   }
   if (shouldInsertScripts){
