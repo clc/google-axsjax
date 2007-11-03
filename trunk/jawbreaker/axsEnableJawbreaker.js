c933dcfbd98138eab8cc83f83f9e62947e9bf6ea
clchen@google.com <clchen@google.com@333d7aac-213b-0410-876c-dd2d68a01fb3>
2007-11-03 01:57:18 +0000

    Added row and col functions

--- trunk/jawbreaker/axsEnableJawbreaker.js
+++ trunk/jawbreaker/axsEnableJawbreaker.js
@@ -3,23 +3,20 @@
 
 var axsJb_row = 0;
 var axsJb_col = 0;
+var axsJb_MAXROW = 11;
+var axsJb_MAXCOL = 10;
 var axsJb_axsJaxObj = new AxsJAX();
 
 
 function axsJb_getCurrentPosition(){
-  if (axsJb_row < 0){
-    axsJb_row = 0;
-  }
-  if (axsJb_row > 11){
-    axsJb_row = 11;
-  }
-  if (axsJb_col < 0){
-    axsJb_col = 0;
-  }
-  if (axsJb_col > 10){
-    axsJb_col = 10;
-  }
-  var ballImg = axsJb_getCurrentBallImgNode();
+  var ballImg = axsJb_getCurrentBallImgNode(axsJb_row,axsJb_col);
+  var color = axsJb_getColorOfBallImg(ballImg);
+  var message = color + axsJb_row + ', ' + axsJb_col + '.';
+  ballImg.alt = message;
+  axsJb_axsJaxObj.speakNode(ballImg);
+}
+
+function axsJb_getColorOfBallImg(ballImg){
   var color = '';
   var ImgSrcString = ballImg.src.toString();
   if (ImgSrcString.indexOf('s_green.gif') != -1){ color = 'Selected. Green. '; }
@@ -33,14 +30,14 @@ function axsJb_getCurrentPosition(){
   if (ImgSrcString.indexOf('p_red.gif') != -1){ color = 'Red. '; }
   if (ImgSrcString.indexOf('p_yellow.gif') != -1){ color = 'Yellow. '; }
   if (ImgSrcString.indexOf('p_white.gif') != -1){ color = 'Blank. '; }
-  var message = color + axsJb_row + ', ' + axsJb_col + '.';
-  ballImg.alt = message;
-  axsJb_axsJaxObj.speakNode(ballImg);
+  return color;
 }
 
-function axsJb_getCurrentBallImgNode(){
-  var rowString = axsJb_row.toString();
-  var colString = axsJb_col.toString();
+
+
+function axsJb_getCurrentBallImgNode(row,col){
+  var rowString = row.toString();
+  var colString = col.toString();
   if (rowString.length < 2){
     rowString = '0' + rowString;
   }
@@ -58,30 +55,58 @@ function axsJb_sayStats(){
   axsJb_axsJaxObj.speakText('Block count: ' + blockCount + '. Block score: ' + blockScore + '. Total score: ' + totalScore + '.');
 }
 
+function axsJb_speakRow(){
+  var speechString = "Row " +  axsJb_row + ": ";
+  for (var col = 0; col < axsJb_MAXCOL; col++){
+    speechString = speechString + axsJb_getColorOfBallImg(axsJb_getCurrentBallImgNode(axsJb_row,col)) + ", ";
+  }
+  speechString = speechString + axsJb_getColorOfBallImg(axsJb_getCurrentBallImgNode(axsJb_row,axsJb_MAXCOL)) + ". ";
+  axsJb_axsJaxObj.speakText(speechString);
+}
+
+function axsJb_speakCol(){
+  var speechString = "Col " +  axsJb_col + ": ";
+  for (var row = 0; row < axsJb_MAXROW; row++){
+    speechString = speechString + axsJb_getColorOfBallImg(axsJb_getCurrentBallImgNode(row,axsJb_col)) + ", ";
+  }
+  speechString = speechString + axsJb_getColorOfBallImg(axsJb_getCurrentBallImgNode(axsJb_MAXROW,axsJb_col)) + ". ";
+  axsJb_axsJaxObj.speakText(speechString);
+}
+
+
 function axsJb_keyboardHandler(evt){
   if (evt.keyCode == 38){ // Up arrow
     axsJb_row--;
+    if (axsJb_row < 0){ axsJb_row = 0; }
     axsJb_getCurrentPosition();
   }
   if (evt.keyCode == 37){ // Left arrow 
     axsJb_col--;
+    if (axsJb_col < 0){ axsJb_col = 0; }
     axsJb_getCurrentPosition();
   }
   if (evt.keyCode == 40){ // Down arrow
     axsJb_row++;
+    if (axsJb_row > axsJb_MAXROW){  axsJb_row = axsJb_MAXROW; }
     axsJb_getCurrentPosition();
   }
   if (evt.keyCode == 39){ // Right arrow
- 
     axsJb_col++;
+    if (axsJb_col > axsJb_MAXCOL){ axsJb_col = axsJb_MAXCOL; }
     axsJb_getCurrentPosition();
   }
   if (evt.charCode == 32){ // Space
-    axsJb_axsJaxObj.clickElem(axsJb_getCurrentBallImgNode());
+    axsJb_axsJaxObj.clickElem(axsJb_getCurrentBallImgNode(axsJb_row,axsJb_col));
     axsJb_getCurrentPosition();
   }
   if (evt.charCode == 115){ // s
     axsJb_sayStats();
+  }
+  if (evt.charCode == 99){ // c
+    axsJb_speakCol();
+  }
+  if (evt.charCode == 114){ // r
+    axsJb_speakRow();
   }
   if (evt.charCode == 110){ // n
     axsJb_row = 0;
