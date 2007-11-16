b7c053f85e97d2fd0e4d8889fdac58c73f655b1c
clchen@google.com <clchen@google.com@333d7aac-213b-0410-876c-dd2d68a01fb3>
2007-11-16 23:48:16 +0000

    Transparent pixel for jawbreaker

--- trunk/jawbreaker/axsEnableJawbreaker.js
+++ trunk/jawbreaker/axsEnableJawbreaker.js
@@ -60,7 +60,7 @@ function axsJb_sayStats(){
   var blockCount = document.getElementById('blockcount').textContent;
   var blockScore = document.getElementById('blockscore').textContent;
   var totalScore = document.getElementById('userscore').textContent;
-  axsJb_axsJaxObj.speakText('Block count: ' + blockCount + '. Block score: ' + blockScore + '. Total score: ' + totalScore + '.');
+  axsJb_axsJaxObj.speakThroughPixel('Block count: ' + blockCount + '. Block score: ' + blockScore + '. Total score: ' + totalScore + '.');
 }
 
 function axsJb_speakRow(){
