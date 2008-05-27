6e9917773137cc07146283a87660c05474efd875
clchen@google.com <clchen@google.com@333d7aac-213b-0410-876c-dd2d68a01fb3>
2008-05-27 19:10:02 +0000

    Fix for tab/activedescendant bug + refactored websearch

--- websearch/axsEnableWebSearch.js
+++ websearch/axsEnableWebSearch.js
@@ -21,32 +21,48 @@
  * @author clchen@google.com (Charles L. Chen)
  */
 
-
-
-
-
-
-// create namespace
+// Create namespace
 var axsWebSearch = {};
 
-
-axsWebSearch.ACCESSIBLE_SEARCH_URL = 
+// Strings for the URLs
+/**
+ * @type {string}
+ */
+axsWebSearch.ACCESSIBLE_SEARCH_URL =
     'http://www.google.com/cse?cx=000183394137052953072%3Azc1orsc6mbq&q=';
-
+/**
+ * @type {string}
+ */
 axsWebSearch.WEB_SEARCH_URL = 'http://www.google.com/search?q=';
 
 //These are strings to be spoken to the user
+/**
+ * @type {string}
+ */
 axsWebSearch.HELP_STRING_PRE = 'The following shortcut keys are available. ';
-axsWebSearch.HELP_STRING_POST =  'Slash, enter search field. ' +
-                                 'Escape, leave search field. ' +
-                                 'Equals, enlarge the current item. ' +
-                                 'Minus, shrink the current item. ';
-
+/**
+ * @type {string}
+ */
+axsWebSearch.HELP_STRING_POST = 'Slash, enter search field. ' +
+                                'Escape, leave search field. ' +
+                                'Equals, enlarge the current item. ' +
+                                'Minus, shrink the current item. ';
+/**
+ * @type {string}
+ */
 axsWebSearch.PAGECONTENT_RELATED_SEARCH_STRING = 'Searches related to:';
-
+/**
+ * @type {string}
+ */
 axsWebSearch.ON_ACCESSIBLE_SEARCH =
     'You are already on Google accessible search.';
+/**
+ * @type {string}
+ */
 axsWebSearch.ON_WEB_SEARCH = 'You are already on Google web search.';
+/**
+ * @type {string}
+ */
 axsWebSearch.SPONSORED_LINK = 'Sponsored link. ';
 
 
@@ -68,8 +84,17 @@ axsWebSearch.axsNavObj = null;
  * @type AxsLens?
  */
 axsWebSearch.axsLensObj = null;
+
+/**
+ * The AxsLens object that will magnify content.
+ * @type number
+ */
 axsWebSearch.magSize = 1.5;
 
+/**
+ * Initializes the web search script by starting up the AxsNav object
+ * and attaching keyboard handlers. 
+ */
 axsWebSearch.init = function(){
   axsWebSearch.axsJAXObj = new AxsJAX(true);
   axsWebSearch.axsNavObj = new AxsNav(axsWebSearch.axsJAXObj);
@@ -82,7 +107,8 @@ axsWebSearch.init = function(){
   axsWebSearch.formatAdAreaSide();
 
   var cnrString = "<cnr next='RIGHT l' prev='LEFT h'>" +
-      "<list title='One Box' hotkey='1' next='DOWN j' prev='UP k' onEmpty='There is no one box on this page.'>" +
+      "<list title='One Box' hotkey='1' next='DOWN j' prev='UP k' " +
+      "onEmpty='There is no one box on this page.'>" +
       "<item count='1'>" +
       "id('res')/p[*]" +
       "</item>" +
@@ -90,9 +116,11 @@ axsWebSearch.init = function(){
       "id('res')/div[@class='e']" +
       "</item>" +
       "</list>" +
-      "<list title='Results' hotkey='n' next='DOWN j' prev='UP k' fwd='n' back='p'>" +
+      "<list title='Results' hotkey='n' next='DOWN j' prev='UP k' " +
+      "fwd='n' back='p'>" +
       "<item count='1'>" +
-      "id('res')//td[@class='j']/ul/li[text()='Make sure all words are spelled correctly.']/../.." +
+      "id('res')//td[@class='j']/ul/li[text()='Make sure all words are " +
+      "spelled correctly.']/../.." +
       "</item>" +
       "<item>" +
       "id('res')//div[@class='g']" +
@@ -104,7 +132,8 @@ axsWebSearch.init = function(){
       "id('np')/.." +
       "</target>" +
       "</list>" +
-      "<list title='Sponsored Links' hotkey='a' next='DOWN j' prev='UP k' onEmpty='There are no sponsored links on this page.'>" +
+      "<list title='Sponsored Links' hotkey='a' next='DOWN j' prev='UP k' " +
+      "onEmpty='There are no sponsored links on this page.'>" +
       "<item>" +
       "id('tads')/div[*]" +
       "</item>" +
@@ -112,7 +141,8 @@ axsWebSearch.init = function(){
       "id('mbEnd')/tbody/tr[*]/td/span[*]" +
       "</item>" +
       "</list>" +
-      "<list title='Related Searches' hotkey='s' next='DOWN j' prev='UP k' onEmpty='There are no related searches.'>" +
+      "<list title='Related Searches' hotkey='s' next='DOWN j' prev='UP k' " +
+      "onEmpty='There are no related searches.'>" +
       "<item>" +
       "id('res')/table[*]/tbody/tr[*]/td[*]/a" +
       "</item>" +
@@ -120,7 +150,9 @@ axsWebSearch.init = function(){
       "body/table[*]/tbody/tr[*]/td[*]/a/b/.." +
       "</item>" +
       "</list>" +
-      "<list title='Alternate Search Categories' hotkey='c' next='DOWN j' prev='UP k' onEmpty='There are no other categories to search within.'>" +
+      "<list title='Alternate Search Categories' hotkey='c' next='DOWN j' " +
+      "prev='UP k' " +
+      "onEmpty='There are no other categories to search within.'>" +
       "<item>" +
       "body/table[*]/tbody/tr/td[*]/font/a[@class='q']" +
       "</item>" +
@@ -143,81 +175,26 @@ axsWebSearch.init = function(){
 
   //Read the first thing on the page.
   //Use a set time out just in case the browser is not entirely ready yet.
-  window.setTimeout(axsWebSearch.readTheFirstThing,100);
+  window.setTimeout(axsWebSearch.readTheFirstThing, 100);
 };
 
 /**
  * Reads the first thing on the page.
  */
-axsWebSearch.readTheFirstThing = function(evt){
+axsWebSearch.readTheFirstThing = function(){
   var firstElem = axsWebSearch.axsNavObj.nextItem().elem;
   axsWebSearch.axsLensObj.view(firstElem);
   axsWebSearch.axsJAXObj.goTo(firstElem);
 };
 
-axsWebSearch.extraKeyboardNavHandler = function(evt){
-  if (evt.ctrlKey){ //None of these commands involve Ctrl.
-                    //If Ctrl is held, it must be for some AT. 
-    return true;
-  }
-  if (evt.keyCode == 27){ // ESC
-    axsWebSearch.axsJAXObj.lastFocusedNode.blur();
-    return false;
-  }
-  if (axsWebSearch.axsJAXObj.inputFocused){
-    return true;
-  }
-
-  if (evt.charCode == 65){ // A
-    axsWebSearch.switchToAccessibleSearch();
-    return false;
-  }
-  if (evt.charCode == 87){ // W
-    axsWebSearch.switchToWebSearch();
-    return false;
-  }
-  if (evt.charCode == 47){ // / (slash symbol)
-    // Focus on the top search blank
-    document.getElementsByName('q')[0].focus();  
-    document.getElementsByName('q')[0].select(); //and select all text
-    return false;
-  }
-  if (evt.keyCode == 13){ // Enter
-    if (evt.shiftKey){
-      axsWebSearch.goToCurrentLinkInNewWindow();
-    } else{
-      axsWebSearch.goToCurrentLink();
-    }
-    return false;
-  }
-
-  if (evt.charCode == 45){ // - (minus symbol)
-    axsWebSearch.decreaseMagnification();
-    return false;
-  }
-
-  if (evt.charCode == 61){ // = (equal symbol) 
-    axsWebSearch.increaseMagnification();
-    return false;
-  }
-
-
-
-  if (evt.charCode == 63){ // ? (question mark)
-    var helpStr = axsWebSearch.HELP_STRING_PRE +
-                  axsWebSearch.axsNavObj.localHelpString() +
-                  axsWebSearch.HELP_STRING_POST;
-    axsWebSearch.axsJAXObj.speakTextViaNode(helpStr);
-    return false;
-  }
-
-  return true;
-};
 
 //************
 //Functions for handling links
 //************
 
+/**
+ * Opens the current URL
+ */
 axsWebSearch.goToCurrentLink = function(){
   var linkUrl = axsWebSearch.getCurrentLink();
   if (linkUrl === ''){
@@ -226,6 +203,9 @@ axsWebSearch.goToCurrentLink = function(){
   document.location = linkUrl;
 };
 
+/**
+ * Opens the current URL link in a new window
+ */
 axsWebSearch.goToCurrentLinkInNewWindow = function(){
   var linkUrl = axsWebSearch.getCurrentLink();
   if (linkUrl === ''){
@@ -234,6 +214,10 @@ axsWebSearch.goToCurrentLinkInNewWindow = function(){
   window.open(linkUrl);
 };
 
+/**
+ * Retrieves the current URL link
+ * @return {string} The current URL hyperlink as a string
+ */
 axsWebSearch.getCurrentLink = function(){
   var currentItem = axsWebSearch.axsNavObj.currentItem();
   if (typeof(currentItem) == 'undefined'){
@@ -252,26 +236,21 @@ axsWebSearch.getCurrentLink = function(){
 //Functions for Ads
 //************
 
-//The Ads area on the right side is inside one big FONT tag.
-//There is no structure that groups the individual ads.
-//Add this structure to make it possible to speak these ads individually.
+/**
+ * Formats the sponsored links section on the right of the search results.
+ * The Ads area on the right side is inside one big FONT tag.
+ * There is no structure that groups the individual ads.
+ * Add this structure to make it possible to speak these ads individually.
+ */
 axsWebSearch.formatAdAreaSide = function(){
   var adTable = window.content.document.getElementById('mbEnd');
-  if(!adTable){
+  if (!adTable){
     return;
   }
-  var adArea = null; //This is the FONT tag that contains all the ads
-                     //However, it is non-trivial to locate as the TR
-                     //that it is under is not always the same TR.
-                     //There are also very few identifiers here to key off of.
-  //This is an ugly way to find the adArea, but it works reliably.
-  var spanArray = adTable.getElementsByTagName('SPAN');
-  for (var i=0, currentSpan; currentSpan = spanArray[i]; i++){
-    if (currentSpan.className == 'a'){
-      adArea = currentSpan.parentNode;
-      break;
-    }
-  }
+  //This is the FONT tag that contains all the ads
+  var xpath = ".//span[@class='a']/..";
+  var adArea = axsWebSearch.axsJAXObj.evalXPath(xpath, adTable)[0];
+
   if (!adArea){
     return;
   }
@@ -281,10 +260,10 @@ axsWebSearch.formatAdAreaSide = function(){
 
   while (currentAdAnchor){
     var nextAnchor = currentAdAnchor.nextSibling;
-    if (currentAdAnchor.tagName &&  currentAdAnchor.tagName == 'FONT'){
+    if (currentAdAnchor.tagName && currentAdAnchor.tagName == 'FONT'){
       var newAdSpan = window.content.document.createElement('span');
       newAdSpan.appendChild(currentAdAnchor.cloneNode(true));
-      adArea.replaceChild(adSpan,currentAdAnchor);
+      adArea.replaceChild(adSpan, currentAdAnchor);
       adSpan = newAdSpan;
     } else {
       adSpan.appendChild(currentAdAnchor.cloneNode(true));
@@ -303,19 +282,26 @@ axsWebSearch.formatAdAreaSide = function(){
 //Functions switching to accessible search experiment
 //************
 
+/**
+ * Retrieves the query from the current URL
+ * @return {string} The query part of the current URL string
+ */
 axsWebSearch.getCurrentURLQueryString = function(){
   var URL = document.baseURI;
   var searchQueryStart = URL.indexOf('q=') + 2;
-  var searchQueryEnd = URL.indexOf('&',searchQueryStart);
+  var searchQueryEnd = URL.indexOf('&', searchQueryStart);
   if (searchQueryEnd == -1){
-    searchQueryEnd = URL.length; 
+    searchQueryEnd = URL.length;
   }
   return URL.substring(searchQueryStart, searchQueryEnd);
 };
 
+/**
+ * Switches to accessible web search
+ */
 axsWebSearch.switchToAccessibleSearch = function(){
-  if ( (document.baseURI.indexOf('http://www.google.com/custom') === 0) ||
-       (document.baseURI.indexOf('http://www.google.com/cse') === 0) ){
+  if ((document.baseURI.indexOf('http://www.google.com/custom') === 0) ||
+      (document.baseURI.indexOf('http://www.google.com/cse') === 0)){
     axsWebSearch.axsJAXObj.speakTextViaNode(axsWebSearch.ON_ACCESSIBLE_SEARCH);
     return;
   }
@@ -323,6 +309,9 @@ axsWebSearch.switchToAccessibleSearch = function(){
   document.location = axsWebSearch.ACCESSIBLE_SEARCH_URL + searchQuery;
 };
 
+/**
+ * Switches to regular web search
+ */
 axsWebSearch.switchToWebSearch = function(){
   if (document.baseURI.indexOf('http://www.google.com/search') === 0){
     axsWebSearch.axsJAXObj.speakTextViaNode(axsWebSearch.ON_WEB_SEARCH);
@@ -333,21 +322,99 @@ axsWebSearch.switchToWebSearch = function(){
 };
 
 
-
 //************
 //Functions for highlighting results
 //************
 
+/**
+ * Increases the magnification by 10%
+ */
 axsWebSearch.increaseMagnification = function(){
   axsWebSearch.magSize += 0.10;
   axsWebSearch.axsLensObj.setMagnification(axsWebSearch.magSize);
 };
 
+/**
+ * Decreases the magnification by 10%
+ */
 axsWebSearch.decreaseMagnification = function(){
   axsWebSearch.magSize -= 0.10;
   axsWebSearch.axsLensObj.setMagnification(axsWebSearch.magSize);
 };
 
+//************
+//Functions for keyboard handling
+//************
+
+/**
+ * Provides keyboard handling
+ * @param {event} evt A keypress event
+ * @return {boolean} Indicates whether the keypress was handled
+ */
+axsWebSearch.extraKeyboardNavHandler = function(evt){
+  if (evt.ctrlKey){ //None of these commands involve Ctrl.
+                    //If Ctrl is held, it must be for some AT. 
+    return true;
+  }
+  if (evt.keyCode == 27){ // ESC
+    axsWebSearch.axsJAXObj.lastFocusedNode.blur();
+    return false;
+  }
+  if (axsWebSearch.axsJAXObj.inputFocused){
+    return true;
+  }
+  if (evt.keyCode == 13){ // Enter
+    if (evt.shiftKey){
+      axsWebSearch.goToCurrentLinkInNewWindow();
+    } else{
+      axsWebSearch.goToCurrentLink();
+    }
+    return false;
+  }
+  
+  var command =  axsWebSearch.charCodeMap[evt.charCode];
+
+  if (command)  return  command();
+
+  return true;
+};
+
+axsWebSearch.charCodeMap = {
+  // ? (question mark)
+  63 : function () {
+         var helpStr = axsWebSearch.HELP_STRING_PRE +
+                       axsWebSearch.axsNavObj.localHelpString() +
+                       axsWebSearch.HELP_STRING_POST;
+                       axsWebSearch.axsJAXObj.speakTextViaNode(helpStr);
+	     return false;
+       }, 
+  // - (minus symbol)
+  45 : function () {
+         axsWebSearch.decreaseMagnification();
+	     return false;
+       }, 
+  // = (equal symbol) 	   
+  61 : function () {
+         axsWebSearch.increaseMagnification();
+	     return false;
+       }, 
+  // / (slash symbol)	   
+  47 : function () {
+         document.getElementsByName('q')[0].focus();
+         document.getElementsByName('q')[0].select();
+	     return false;
+       }, 
+  // A	   
+  65 : function () {
+         axsWebSearch.switchToAccessibleSearch();
+	     return false;
+       },
+  // W	   
+  87 : function () {
+         axsWebSearch.switchToWebSearch();
+	     return false;
+       }
+};
 
 
 axsWebSearch.init();
