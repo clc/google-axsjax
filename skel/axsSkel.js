0dc87af6dbecf1ece50cba0066624252db1210d9
clchen@google.com <clchen@google.com@333d7aac-213b-0410-876c-dd2d68a01fb3>
2008-08-14 20:01:04 +0000

    Cleaned up axsskel

--- skel/axsSkel.js
+++ skel/axsSkel.js
@@ -25,7 +25,10 @@
 // create namespace
 var axsSkel = {};
 
-//These are strings to be spoken to the user
+/**
+ * These are strings to be spoken to the user
+ * @type string
+ */
 axsSkel.HELP = 'The following shortcut keys are available. ';
 
 
@@ -58,15 +61,22 @@ axsSkel.axsSoundObj = null;
  */
 axsSkel.magSize = 1.5;
 
+/**
+ * Initializes the AxsJAX script
+ */
 axsSkel.init = function(){
   axsSkel.axsJAXObj = new AxsJAX(true);
   axsSkel.axsNavObj = new AxsNav(axsSkel.axsJAXObj);
 
   //Add event listeners
-  document.addEventListener('DOMNodeInserted', axsSkel.nodeInsertedHandler, true);
-  document.addEventListener('DOMAttrModified', axsSkel.attrModifiedHandler, true);
+  document.addEventListener('DOMNodeInserted',
+                            axsSkel.nodeInsertedHandler,
+                            true);
+  document.addEventListener('DOMAttrModified',
+                            axsSkel.attrModifiedHandler,
+                            true);
   document.addEventListener('keypress', axsSkel.keyHandler, true);
-  
+
   var cnrString = 'PUT THE CNR XML HERE';
 
   axsSkel.axsNavObj.navInit(cnrString, null);
@@ -76,17 +86,25 @@ axsSkel.init = function(){
   axsSkel.axsLensObj.setMagnification(axsSkel.magSize);
   axsSkel.axsSoundObj = new AxsSound(true);
   axsSkel.axsNavObj.setSound(axsSkel.axsSoundObj);
-  
+
   //Delete the next line when you are done with your script.
   alert('AxsSkel loaded and initialized!');
 };
 
+/**
+ * Handler for DOMNodeInserted events. 
+ * @param {Object} evt A DOMNodeInserted event
+ */
 axsSkel.nodeInsertedHandler = function(evt){
   var target = evt.target;
   // If the target node is something that should
   // be spoken, speak it here.
 };
 
+/**
+ * Handler for DOMAttrModified events. 
+ * @param {Object} evt A DOMAttrModified event
+ */
 axsSkel.attrModifiedHandler = function(evt){
   var attrib = evt.attrName;
   var newVal = evt.newValue;
@@ -96,50 +114,62 @@ axsSkel.attrModifiedHandler = function(evt){
   // be spoken, speak it here.
 };
 
-
+/**
+ * Handler for key events. 
+ * @param {Object} evt A keypress event
+ * @return {boolean} If true, the event should be propagated.
+ */
 axsSkel.keyHandler = function(evt){
   //If Ctrl is held, it must be for some AT. 
   if (evt.ctrlKey) return true;
-  
+
   if (evt.keyCode == 27){ // ESC
     axsSkel.axsJAXObj.lastFocusedNode.blur();
     return false;
   }
 
   if (axsSkel.axsJAXObj.inputFocused) return true;
-  
-  var command =  axsSkel.keyCodeMap[evt.keyCode] ||
-                 axsSkel.charCodeMap[evt.charCode];
 
-  if (command)  return  command();
+  var command = axsSkel.keyCodeMap[evt.keyCode] ||
+                axsSkel.charCodeMap[evt.charCode];
+
+  if (command) return command();
+
   return true;
-  
 };
 
+/**
+ * Map from key codes to functions
+ */
 axsSkel.keyCodeMap = {
   // Map additional keyboard behavior that involves key codes here
 };
 
+/**
+ * Map from character codes to functions
+ * @return {boolean} Always returns false to indicate 
+ *                   that the keycode has been handled.
+ */
 axsSkel.charCodeMap = {
   // Map additional keyboard behavior that involves char codes here
   // - (minus symbol)
-  45 : function () {
+  45 : function() {
          axsSkel.magSize -= 0.10;
          axsSkel.axsLensObj.setMagnification(axsSkel.magSize);
-	     return false;
+         return false;
        },
-  // = (equal symbol) 	   
-  61 : function () {
+  // = (equal symbol)
+  61 : function() {
          axsSkel.magSize += 0.10;
          axsSkel.axsLensObj.setMagnification(axsSkel.magSize);
-	     return false;
+         return false;
        },
-  63 : function () {
+  63 : function() {
          var helpStr = axsSkel.HELP +
                        axsSkel.axsNavObj.localHelpString() +
-					   axsSkel.axsNavObj.globalHelpString();
+                       axsSkel.axsNavObj.globalHelpString();
          axsSkel.axsJAXObj.speakTextViaNode(helpStr);
-	     return false;
+         return false;
        }
 };
 
