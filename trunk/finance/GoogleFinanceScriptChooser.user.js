2bad503781afe1817692ebcd23d282cdc8fef221
svetoslavganov@google.com <svetoslavganov@google.com@333d7aac-213b-0410-876c-dd2d68a01fb3>
2010-05-13 03:10:01 +0000

    Test

--- trunk/finance/GoogleFinanceScriptChooser.user.js
+++ trunk/finance/GoogleFinanceScriptChooser.user.js
@@ -24,6 +24,7 @@ function pickScript() {
   scriptUrlsArray.push(baseURL + 'common/AxsLens.js');
   scriptUrlsArray.push(baseURL + 'common/AxsSound.js');
   scriptUrlsArray.push(baseURL + 'common/PowerKey.js');
+  scriptUrlsArray.push(baseURL + 'accessibility.js');
 
   var currentURL = document.baseURI;
   var path = document.location.pathname;
